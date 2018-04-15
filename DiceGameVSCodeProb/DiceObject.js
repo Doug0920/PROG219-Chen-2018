e599ee95213d7dcd33c3e5eb85c9f888554c83fd
Doug0920 <32145490+Doug0920@users.noreply.github.com>
2018-04-14 21:06:26 -0700

    small changes; avoid global var use inside objects

--- DiceGameVSCodeProb/DiceObject.js
+++ DiceGameVSCodeProb/DiceObject.js
@@ -1,21 +1,20 @@
 // Dice object constructor
+// (objects cannot depend on global variables/values)
 
 let Dice=function(){//dice constructor
     this.rolling=function(inputRollingTime,$inputImageRef){//inputRollingTime is rolling time, $inputImageRef is jQuery DOM object for show the dice image
-        this.point=Math.floor(Math.random() * 6) + 1;
-        this.url="./images/dice-" + (this.point).toString() + ".jpg";
         if(inputRollingTime<51){//for make sure dice rolling time more than setInterval time
             inputRollingTime=51;
         }
         let diceRolling =setInterval((
             ()=>{
-                let rollingImage="./images/dice-" + (Math.floor(Math.random() * 6) + 1).toString() + ".jpg";
+                this.point=Math.floor(Math.random() * 6) + 1;	// Use last as point used
+                rollingImage="./images/dice-" + (this.point).toString() + ".jpg";
                 $inputImageRef.attr("src",rollingImage);
             }
         ), 50);
         setTimeout(()=>{
             clearInterval(diceRolling);
-            $inputImageRef.attr("src",this.url);
         },inputRollingTime);
     }
 }
--- DiceGameVSCodeProb/PlayerObjects.js
+++ DiceGameVSCodeProb/PlayerObjects.js
@@ -1,20 +1,26 @@
-// Dice object constructor
-let Player=function(){
-    this.name= 'Player1';
+// Player object constructor
+// (objects cannot depend on global variables/values)
+
+let Player=function(pName){
+    this.name= pName;
     this.round=0;
     this.balance=5;
+    //		I will move PlayThisRound into Game object
     this.playThisRound=function(dice1,dice2){//update status information, round information, balance information
         let $status=$("#status");
         let $balance=$("#balance");
         let $turnCount=$("#turnCount");
         this.round=this.round+1;
         $turnCount.text(this.round);//update round status
+/* changed from week 1 to week 2
         if(dice1===dice2){//when 2 random numbers have the same value, balance add 1
             $status.css("color", "red");
             $status.text("A pair of "+dice1+"s, you win this round.");//update win this round information 
             this.balance=this.balance+1;
         }
-        else if(dice1+dice2===7){//when sum of the 2 random numbers equals 7, balance add 1
+        else 
+        */
+        if(dice1+dice2===7){//when sum of the 2 random numbers equals 7, balance add 1
             $status.css("color", "red");
             $status.text("Lucky 7, you win this round.");//update win this round information
             this.balance=this.balance+1;
