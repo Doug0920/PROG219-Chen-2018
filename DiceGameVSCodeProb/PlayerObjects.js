e8f0d7803d729b8314e62f4de5f9907a41f175ad
taofeichen@gmail.com <taofeichen@gmail.com>
2018-04-15 12:16:08 -0700

    update comment, update css show who is the player for this round, move code to game object

--- DiceGameVSCodeProb/PlayerObjects.js
+++ DiceGameVSCodeProb/PlayerObjects.js
@@ -2,37 +2,8 @@
 // (objects cannot depend on global variables/values)
 
 let Player=function(pName,$inputPlayerRef){
-    if(pName==null){
-        this.name="I am a robot"
-    }
-    else{
-        this.name= pName;
-    }
+    this.name= pName;
     this.round=0;
     this.balance=5;
     this.$playerRef=$inputPlayerRef;
-    this.playThisRound=function(dice1Point,dice2Point){//update status information, round information, balance information
-        let $status=$("#status");
-        let $balance=this.$playerRef.find(".balance");
-        let $turnCount=this.$playerRef.find(".turnCount");
-        this.round=this.round+1;
-        $turnCount.text(this.round);//update round status
-        if(dice1Point+dice2Point===7){//when sum of the 2 random numbers equals 7, balance add 1
-            $status.css("color", "red");
-            $status.text("Lucky 7, you win this round.");//update win this round information
-            this.balance=this.balance+1;
-        }
-        else if(dice1Point+dice2Point===11){//when sum of the 2 random numbers equals 11, balance add 1
-            $status.css("color", "red");
-            $status.text("Lucky 11, you win this round.");//update win this round information
-            this.balance=this.balance+1;
-        }
-        else{//other situation balance minus 1
-            $status.css("color", "green");
-            $status.text("Total is "+(dice1Point+dice2Point)+". You lost this round.");//update lost this round information
-            this.balance=this.balance-1;
-        }
-        $status.prepend(this.name+". ");//add player name in status.
-        $balance.text(this.balance);//update balance status
-    }
 }
--- DiceGameVSCodeProb/main.js
+++ DiceGameVSCodeProb/main.js
@@ -1,33 +1,47 @@
-$("#status").text("Welcome!");//add welcome message
 $( document ).ready(function() {
     let rollingTime=1000;//rolling animation time
     let $image1=$("#image1");
     let $image2=$("#image2");
     let $buttonBet=$("#ButtonBet");
-    let diceGame={
-    }
+    //add three players in the game.
     let player1=new Player("AlwaysWin",$("#player1"));
     let player2=new Player("AlwaysLose",$("#player2"));
     let player3=new Player("AlwaysTie",$("#player3"));
-    diceGame.players=[player1,player2,player3];
-    for(let i=0;i<diceGame.players.length;i++){//update player name in each player status
+    diceGame.players.push(player1);
+    diceGame.players.push(player2);
+    diceGame.players.push(player3);
+    //add welcome message for three player.
+    $("#status").text("Welcome!");
+    //update player name in each player status
+    for(let i=0;i<diceGame.players.length;i++){
         diceGame.players[i].$playerRef.find(".playerName").text(diceGame.players[i].name);
+        $("#status").append(" "+diceGame.players[i].name+".");
     }
-    diceGame.currentPlayerPointer=0;//start pointer with first player
     $(".playerName").css( "color", "purple" );
-    $buttonBet.click(function() {//click event on the "ButtonBet" button
+    //click event on the "ButtonBet" button
+    $buttonBet.click(function() {
         let dice1=new Dice();
         dice1.rolling(rollingTime,$image1);
         let dice2=new Dice();
         dice2.rolling(rollingTime,$image2);
         $buttonBet.prop('disabled', true);//disable the "ButtonBet" button
+        //change last player background color back to lightblue, change this round background color to white
+        let lastPlayerPointer=diceGame.currentPlayerPointer-1;
+        if(diceGame.currentPlayerPointer-1<0){
+            lastPlayerPointer=diceGame.players.length-1;
+        }
+        diceGame.players[lastPlayerPointer].$playerRef.css("background-color","lightblue");
+        diceGame.players[diceGame.currentPlayerPointer].$playerRef.css("background-color","white");
         setTimeout(()=>{//game running result, update display information
             console.log("first dice is ",dice1.point);
             console.log("second dice is ",dice2.point);
             //any player's balance is 0, will out the game.
-            diceGame.players[diceGame.currentPlayerPointer].playThisRound(dice1.point,dice2.point);
-            if(diceGame.players[diceGame.currentPlayerPointer].balance===0){//if player's balance is zero remove from the players array
-            diceGame.players.splice(diceGame.currentPlayerPointer,1);
+            diceGame.playThisRound(dice1.point,dice2.point,diceGame.players[diceGame.currentPlayerPointer]);//play one round
+            if(diceGame.players[diceGame.currentPlayerPointer].balance===0){//if player's balance is zero remove from the players array,update players status
+            diceGame.players[diceGame.currentPlayerPointer].$playerRef.css("background-color","black");
+            diceGame.players[diceGame.currentPlayerPointer].$playerRef.css("color","white");
+            diceGame.players[diceGame.currentPlayerPointer].$playerRef.append("<b style=\"color:yellow\">+++++++>Game Overed!<+++++++</b>");
+            diceGame.players.splice(diceGame.currentPlayerPointer,1);//remove currentplayer from the players
             }
             else{//move to the next player
                 diceGame.currentPlayerPointer++;
